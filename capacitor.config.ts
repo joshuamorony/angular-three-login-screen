80f2d4284e5b68fb9aa0e7b9501e32073cdc6775
Joshua Morony <joshua.morony@gmail.com>
2021-12-08 20:02:57 +1030

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular-three-login-screen',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
